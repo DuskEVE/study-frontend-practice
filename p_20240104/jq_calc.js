4884e2a7a5ffaa67b6206a1c24456c499817b76b
DuskEVE <x987000@gmail.com>
2024-01-04 14:19:07 +0800

    minor change

--- p_20240104/jq_calc.js
+++ p_20240104/jq_calc.js
@@ -17,35 +17,33 @@ function calc(event){
 
 function addRow(){
     let id = calcBtns.length + 1;
-    let newElementName = `calc-${String(id).padStart(2, '0')}`;
+    let newClassName = `calc-${String(id).padStart(2, '0')}`;
     let calcUnit = `
-                    <div class="row ${newElementName} m-3">
+                <div class="row ${newClassName} m-3">
                     <div class="col-3">
-                        <input type="number" name="num1" id="num1" class="${newElementName}-num1 form-control">
+                        <input type="number" name="num1" id="num1" class="${newClassName}-num1 form-control">
                     </div>
                     <div class="col-3">
-                        <input type="number" name="num2" id="num2" class="${newElementName}-num2 form-control">
+                        <input type="number" name="num2" id="num2" class="${newClassName}-num2 form-control">
                     </div>
                     <div class="col-2">
-                        <select name="option" id="option" class="${newElementName}-option form-control">
+                        <select name="option" id="option" class="${newClassName}-option form-control">
                             <option value="">加</option>
                             <option value="">減</option>
                             <option value="">乘</option>
                             <option value="">除</option>
                         </select>
                     </div>
-                    <div class="col-2"><button id="calc" class="btn btn-success calc-btn ${newElementName}-calc" data-target=".${newElementName}">計算</button></div>
+                    <div class="col-2"><button id="calc" class="btn btn-success calc-btn ${newClassName}-calc" data-target=".${newClassName}">計算</button></div>
                     <div class="col-2">
-                        <input type="number" name="result" id="result" class="${newElementName}-result form-control">
+                        <input type="number" name="result" id="result" class="${newClassName}-result form-control">
                     </div>
                 </div>
     `;
     container.append(calcUnit);
     calcBtns = $('.calc-btn');
-    $(`.${newElementName}-calc`).on('click', calc);
+    $(`.${newClassName}-calc`).on('click', calc);
 }
 
-calcBtns.each((index, element) => {
-    $(element).on('click', calc);
-});
+calcBtns.on('click', calc);
 addBtn.on('click', addRow);
