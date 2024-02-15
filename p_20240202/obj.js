a427150e7a0c8413984a9d63a5549e77fd694010
DuskEVE <x987000@gmail.com>
2024-02-15 15:10:03 +0800

    js practice

---
+++ p_20240202/obj.js
@@ -0,0 +1,42 @@
+let obj = {
+ "cwaopendata": {
+  "@xmlns": "urn:cwa:gov:tw:cwacommon:0.1",
+  "identifier": "f8df59bc-9ef0-1b54-62f9-f9a354def4ce",
+  "sender": "weather@cwa.gov.tw",
+  "sent": "2024-02-02T07:31:08+08:00",
+  "status": "Actual",
+  "msgType": "Issue",
+  "scope": "Public",
+  "dataid": "F-C0032-009",
+  "source": "Weather Forecast Center",
+  "dataset": {
+   "datasetInfo": {
+    "datasetDescription": "WeatherAssistant",
+    "datasetLanguage": "zh-TW",
+    "issueTime": "2024-02-02T07:28:36+08:00"
+   },
+   "location": {
+    "locationName": "台北市",
+    "stationId": "46692",
+    "geocode": "63"
+   },
+   "parameterSet": {
+    "parameterSetName": "天氣小幫手描述",
+    "parameter": [
+     {
+      "parameterValue": "東北季風稍增強，氣溫稍下降，有局部短暫雨"
+     },
+     {
+      "parameterValue": "昨天（１）日天氣多雲到晴；氣溫方面，清晨稍涼，中午有點悶熱，臺北站截至下午五點，測得最低溫17.2度，最高溫29.6度。"
+     },
+     {
+      "parameterValue": "今(２)日東北季風稍增強，氣溫稍下降，平地氣溫約18-20度；而天氣則為多雲時陰短暫雨的型態，降雨機率約30%；明（３）日東北季風影響，北部及東北部天氣稍涼；桃園以北有局部短暫雨，；今日白天和明日西半部地區有局部霧或低雲影響能見度，臺灣北部海面及臺灣海峽北部有局部霧，請注意。"
+     },
+     {
+      "parameterValue": "由於鋒面通過及東北風增強，今（２日）晨臺灣北部海面有明顯風變，風向將由偏南風轉為偏北風；臺灣北部海面及臺灣海峽北部平均風力可達６級，最大陣風８級，船隻請注意。明（３）日臺灣北部海面及臺灣海峽平均風力將稍減弱。"
+     }
+    ]
+   }
+  }
+ }
+}
